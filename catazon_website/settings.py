6b1ddd0266a72425c403a0ed5e5738adfb06477a
Nguyễn Tân <king20knight@gmail.com>
2022-03-18 10:39:37 +0700

    add home page

--- catazon_website/settings.py
+++ catazon_website/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'catazon_website/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
