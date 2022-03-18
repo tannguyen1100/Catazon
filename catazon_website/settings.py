758a5cc7ee185d3656e68345c4c19e9ec74e224b
Nguyễn Tân <king20knight@gmail.com>
2022-03-18 15:42:17 +0700

    config fixed port

--- catazon_website/settings.py
+++ catazon_website/settings.py
@@ -132,7 +132,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'catazon_website/static'),
+    os.path.join(BASE_DIR, 'catazon_website/static'), 
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
