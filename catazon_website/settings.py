f8f48a666209a3c62d47ac447edb4db1d66b7020
Nguyễn Tân <king20knight@gmail.com>
2022-03-18 14:59:15 +0700

    deploy home page

--- catazon_website/settings.py
+++ catazon_website/settings.py
@@ -128,13 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = 'catazon_website/static/'
 
+STATIC_URL = '/static/'
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'catazon_website/static'),
 )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Cầu hình media file
 MEDIA_URL = '/media/'
